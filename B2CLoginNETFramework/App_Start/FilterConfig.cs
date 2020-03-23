e60e9943a1c0b8ccb46cfcc6d56a021a2802be3f
Jovan Petkoski <petkoski15@hotmail.com>
2020-03-23 15:45:30 +0100

    Add project files.

---
+++ B2CLoginNETFramework/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace B2CLoginNETFramework
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
